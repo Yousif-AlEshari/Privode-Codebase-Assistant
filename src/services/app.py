5be801704536c54e491f4c20dfd5be6721fbd470
Yousif-AlEshari <yousifaleshari1@gmail.com>
2025-11-11 00:40:51 +0300

    fixed app.py root path

--- src/services/app.py
+++ src/services/app.py
@@ -1,11 +1,11 @@
 # app.py
 import os
+import sys
 sys.path.append(
     os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
             "../..")))
-import sys
 import streamlit as st
 import requests
 import json
